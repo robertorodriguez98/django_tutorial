fd02d58eb7eaad1f9ca9318214491bbefbf51747
root <root@bravo.roberto.gonzalonazareno.org>
2023-01-23 02:24:25 +0000

    cambios para entorno desarrollo

--- django_tutorial/settings.py
+++ django_tutorial/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = '9f0h)gozf$g%6igo8&767w1xro0adm+)msxe)!eic$!fhvynb8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
+STATIC_ROOT = '/var/www/html/django_tutorial/static/'
 
 # Application definition
 
