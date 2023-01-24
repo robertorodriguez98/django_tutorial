5b9ef4cb8d181e428395bd8b941eeae7766da879
Roberto Rodríguez Márquez <robertorodriguezmarquez98@gmail.com>
2023-01-24 12:23:36 +0000

    creada categoria cantidad

--- polls/models.py
+++ polls/models.py
@@ -30,6 +30,7 @@ class Choice(models.Model):
 class Categoria(models.Model):
     Abr = models.CharField(max_length=4)
     Nombre = models.CharField(max_length=50)
+    Cantidad = models.IntegerField(default=0)
 
     def __str__(self):
         return self.Abr+" - "+self.Nombre
